41c8ae06d3a661bca60bad540949675a5d54fda4
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-02-04 11:25:21 +0530

    Enable micronaut.library plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("io.micronaut.application") version "4.6.1"
-    #id("io.micronaut.library")
+    id("io.micronaut.library") version "4.6.1"
     id("com.gradleup.shadow") version "8.3.9"
 }
 
