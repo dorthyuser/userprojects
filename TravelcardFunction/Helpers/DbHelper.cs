2ddec474720cc4fdc70c32e0ae0ada1597ccc8cb
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-03-25 23:32:26 +0530

    Cast enums in SQL insert statements for travelcards

--- TravelcardFunction/Helpers/DbHelper.cs
+++ TravelcardFunction/Helpers/DbHelper.cs
@@ -32,49 +32,53 @@ namespace TravelcardFunction.Helpers
             await using var tx = await conn.BeginTransactionAsync();
             try
             {
-                // Insert travelcard
+                // ✅ FIX 1: cast to enum
                 var travelcardIdSql = @"INSERT INTO public.travelcards (
                     travelcard_type, travelcard_valid_from, travelcard_valid_to, travelcard_name, travelcard_number, travelcard_requested_date, travelcard_transaction_reference, travelcard_usable_to
-                    ) VALUES (@type, @validFrom, @validTo, @name, @number, @requestedDate, @transactionRef, @usableTo) RETURNING id";
+                    ) VALUES (@type::travelcard_type_enum, @validFrom, @validTo, @name, @number, @requestedDate, @transactionRef, @usableTo) RETURNING id";
 
                 await using var cmd = conn.CreateCommand();
                 cmd.CommandText = travelcardIdSql;
-                cmd.Parameters.Add(new NpgsqlParameter("@type", NpgsqlDbType.Varchar) { Value = request.TravelcardType.ToString() });
+
+                cmd.Parameters.AddWithValue("@type", request.TravelcardType.ToString()); // ok now
                 cmd.Parameters.Add(new NpgsqlParameter("@validFrom", NpgsqlDbType.TimestampTz) { Value = request.TravelcardValidFrom });
                 cmd.Parameters.Add(new NpgsqlParameter("@validTo", NpgsqlDbType.TimestampTz) { Value = request.TravelcardValidTo });
                 cmd.Parameters.Add(new NpgsqlParameter("@name", NpgsqlDbType.Varchar) { Value = (object)request.TravelcardName ?? DBNull.Value });
                 cmd.Parameters.Add(new NpgsqlParameter("@number", NpgsqlDbType.Varchar) { Value = request.TravelcardNumber });
                 cmd.Parameters.Add(new NpgsqlParameter("@requestedDate", NpgsqlDbType.TimestampTz) { Value = request.TravelcardRequestedDate });
-                cmd.Parameters.AddWithValue("@transactionRef", request.TravelcardTransactionReference );
+                cmd.Parameters.AddWithValue("@transactionRef", request.TravelcardTransactionReference);
                 cmd.Parameters.Add(new NpgsqlParameter("@usableTo", NpgsqlDbType.TimestampTz) { Value = (object)request.TravelcardUsableTo ?? DBNull.Value });
 
                 var newIdObj = await cmd.ExecuteScalarAsync();
                 var dbId = Convert.ToInt32(newIdObj);
 
-                // Insert cardholders
+                // ✅ FIX 2: cast enum here also
                 var insertCardholderSql = @"INSERT INTO public.cardholders (
                     travelcard_id, cardholder_title, cardholder_forename, cardholder_surname, cardholder_type, cardholder_photo_name, cardholder_photo_rrs_key, cardholder_photo_url, cardholder_photo_key
-                    ) VALUES (@travelcardId, @title, @forename, @surname, @type, @photoName, @photoRrsKey, @photoUrl, @photoKey)";
+                    ) VALUES (@travelcardId, @title, @forename, @surname, @type::cardholder_type_enum, @photoName, @photoRrsKey, @photoUrl, @photoKey)";
 
                 foreach (var ch in request.Cardholders)
                 {
                     await using var cmd2 = conn.CreateCommand();
                     cmd2.CommandText = insertCardholderSql;
+
                     cmd2.Parameters.Add(new NpgsqlParameter("@travelcardId", NpgsqlDbType.Integer) { Value = dbId });
                     cmd2.Parameters.Add(new NpgsqlParameter("@title", NpgsqlDbType.Varchar) { Value = ch.CardholderTitle });
                     cmd2.Parameters.Add(new NpgsqlParameter("@forename", NpgsqlDbType.Varchar) { Value = ch.CardholderForename });
                     cmd2.Parameters.Add(new NpgsqlParameter("@surname", NpgsqlDbType.Varchar) { Value = ch.CardholderSurname });
-                    cmd2.Parameters.Add(new NpgsqlParameter("@type", NpgsqlDbType.Varchar) { Value = ch.CardholderType.ToString() });
+
+                    cmd2.Parameters.AddWithValue("@type", ch.CardholderType.ToString()); // ok now
+
                     cmd2.Parameters.Add(new NpgsqlParameter("@photoName", NpgsqlDbType.Varchar) { Value = ch.CardholderPhotoName });
                     cmd2.Parameters.Add(new NpgsqlParameter("@photoRrsKey", NpgsqlDbType.Varchar) { Value = (object)ch.CardholderPhotoRrsKey ?? DBNull.Value });
                     cmd2.Parameters.Add(new NpgsqlParameter("@photoUrl", NpgsqlDbType.Varchar) { Value = (object)ch.CardholderPhotoUrl ?? DBNull.Value });
                     cmd2.Parameters.Add(new NpgsqlParameter("@photoKey", NpgsqlDbType.Varchar) { Value = (object)ch.CardholderPhotoKey ?? DBNull.Value });
+
                     await cmd2.ExecuteNonQueryAsync();
                 }
 
                 await tx.CommitAsync();
 
-                // Generate GUID and token to return; GUID is external identifier (not stored in schema)
                 var travelcardGuid = Guid.NewGuid().ToString();
                 var token = TokenGenerator.GenerateToken(6);
 
@@ -91,7 +95,6 @@ namespace TravelcardFunction.Helpers
 
         public void Dispose()
         {
-            // NpgsqlDataSource does not need dispose here; kept for interface
         }
     }
 
