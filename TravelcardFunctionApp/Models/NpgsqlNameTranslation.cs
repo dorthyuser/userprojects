3710e8fb2c394acf392a88fa70dd4c5355c68164
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-03-26 02:09:01 +0530

    Delete TravelcardFunctionApp/Models/NpgsqlNameTranslation.cs

--- TravelcardFunctionApp/Models/NpgsqlNameTranslation.cs
+++
@@ -1,9 +0,0 @@
-namespace Npgsql.NameTranslation
-{
-    // Defined to ensure compatibility with Npgsql versions where this interface type is expected at compile time.
-    public interface INpgsqlNameTranslator
-    {
-        string TranslateMemberName(string memberName);
-        string TranslateTypeName(string typeName);
-    }
-}
