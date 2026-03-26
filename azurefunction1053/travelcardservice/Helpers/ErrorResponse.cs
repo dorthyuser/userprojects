7772a137681beb24ce71d57735e4e7e61a03e898
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-03-26 23:04:02 +0530

    Delete azurefunction1053 directory

--- azurefunction1053/travelcardservice/Helpers/ErrorResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace travelcardservice.Helpers
-{
-    public class ErrorResponse
-    {
-        public string ErrorCode { get; set; } = string.Empty;
-        public string Message { get; set; } = string.Empty;
-        public string Details { get; set; } = string.Empty;
-    }
-}
--- azurefunction1053/travelcardservice/Models/DbResult.cs
+++
@@ -1,8 +0,0 @@
-namespace travelcardservice.Models
-{
-    public class DbResult
-    {
-        public int TravelcardId { get; set; }
-        public string Token { get; set; } = string.Empty;
-    }
-}
