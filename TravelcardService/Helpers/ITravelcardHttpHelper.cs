c0ca34eb20f2171d8cc62282c2bc892ba5a346bb
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-04-06 12:59:34 +0530

    Update ITravelcardHttpHelper.cs

--- TravelcardService/Helpers/ITravelcardHttpHelper.cs
+++ TravelcardService/Helpers/ITravelcardHttpHelper.cs
@@ -1,10 +1,10 @@
+using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
 namespace TravelcardService.Helpers
 {
     public interface ITravelcardHttpHelper
     {
-        Task<string> ForwardAsync(string body, ILogger logger);
+        Task<HttpResponseMessage> ForwardAsync(string body);
     }
 }
