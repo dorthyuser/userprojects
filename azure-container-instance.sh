5bb90f427e966d944b40b629f46c39dd4b1ddafa
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-04-19 11:54:01 +0530

    Update azure-container-instance.sh

--- azure-container-instance.sh
+++ azure-container-instance.sh
@@ -60,13 +60,14 @@ for i in $(seq 1 $MAX_RETRIES); do
     --restart-policy Always \
     --assign-identity \
     --environment-variables \
+    --environment-variables \
     AZURE_KEY_VAULT="$AZURE_KEY_VAULT" \
     ASPNETCORE_URLS="http://+:$PORT" \
     ZOHO_BASE_URL="https://www.zohoapis.in" \
-    ZOHO-CLIENT-ID="ZOHO-CLIENT-ID" \
-    ZOHO-CLIENT-SECRET="ZOHO-CLIENT-SECRET" \
-    ZOHO-REFRESH-TOKEN="ZOHO-REFRESH-TOKEN" \
-    ZOHO-TOKEN-URL="ZOHO-TOKEN-URL" \
+    ZOHO_CLIENT_ID="ZOHO-CLIENT-ID" \
+    ZOHO_CLIENT_SECRET="ZOHO-CLIENT-SECRET" \
+    ZOHO_REFRESH_TOKEN="ZOHO-REFRESH-TOKEN" \
+    ZOHO_TOKEN_URL="ZOHO-TOKEN-URL" \
     ZOHO_REDIRECT_URL="ZOHO-REDIRECT-URL" 
   then
     echo "Deployment succeeded"
