b1742a91e44af075225d01108e90a025859edef4
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-04-19 22:00:05 +0530

    Update csharp-docker-build.sh

--- csharp-docker-build.sh
+++ csharp-docker-build.sh
@@ -16,9 +16,7 @@ dotnet restore
 dotnet publish -c Release -o publish
 
 # Find DLL
-PROJECT_FILE=$(find . -name "*.csproj" ! -name "*Tests*" | head -n 1)
-PROJECT_NAME=$(basename "$PROJECT_FILE" .csproj)
-DLL_NAME="$PROJECT_NAME.dll"
+DLL_NAME=$(ls publish/*.runtimeconfig.json | head -n 1 | xargs -n1 basename | sed 's/.runtimeconfig.json/.dll/')
 
 if [ ! -f "publish/$DLL_NAME" ]; then
   echo "ERROR: Main DLL not found!"
