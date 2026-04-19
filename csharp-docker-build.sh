cbceb893a51e766d356512f3f62a4db19062db04
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-04-19 21:57:49 +0530

    Update csharp-docker-build.sh

--- csharp-docker-build.sh
+++ csharp-docker-build.sh
@@ -16,9 +16,9 @@ dotnet restore
 dotnet publish -c Release -o publish
 
 # Find DLL
-DLL_NAME=$(ls publish/*.dll | grep -v "Azure\|Microsoft\|System" | head -n 1 | xargs -n1 basename)
-
-echo "Detected main DLL: $DLL_NAME"
+PROJECT_FILE=$(find . -name "*.csproj" ! -name "*Tests*" | head -n 1)
+PROJECT_NAME=$(basename "$PROJECT_FILE" .csproj)
+DLL_NAME="$PROJECT_NAME.dll"
 
 if [ ! -f "publish/$DLL_NAME" ]; then
   echo "ERROR: Main DLL not found!"
