85f2229cd4cc23ab090f756bba424e17b0e4ce52
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-05-09 20:18:41 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   java
-  id("org.springframework.boot") version "4.0.3"
+  id("org.springframework.boot") version "3.5.0"
   id("io.spring.dependency-management") version "1.1.7"
   id("com.gradleup.shadow") version "8.3.9"
 }
