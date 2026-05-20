42ab3df4fad5dc2928440af18f2de3b7d8e7778c
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2026-05-20 15:46:35 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(o =>
 {
     o.JsonSerializerOptions.PropertyNamingPolicy = null;
-    o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(null, allowIntegerValues: false));
+    o.JsonSerializerOptions.Converters.Add(
+        new JsonStringEnumConverter(null, allowIntegerValues: false));
 });
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
 });
+
+builder.Services.AddHttpClient();
 
 builder.Services.AddSingleton<IZohoHttpConnectionConnection, ZohoHttpConnectionConnection>();
 builder.Services.AddSingleton<IZohoHttpConnectionService, ZohoHttpConnectionService>();
@@ -25,12 +28,16 @@ var database = SecretHelper.Get("POSTGRESQLDATABASE", "POSTGRESQLDATABASE");
 var username = SecretHelper.Get("POSTGRESQLUSERNAME", "POSTGRESQLUSERNAME");
 var password = SecretHelper.Get("POSTGRESQLPASSWORD", "POSTGRESQLPASSWORD");
 
-var connStr = $"Host={host};Port={port};Database={database};Username={username};Password={password};";
+var connStr =
+    $"Host={host};Port={port};Database={database};Username={username};Password={password};";
+
 var dsBuilder = new NpgsqlDataSourceBuilder(connStr);
 var dataSource = dsBuilder.Build();
 
 builder.Services.AddSingleton(dataSource);
 
 var app = builder.Build();
+
 app.MapControllers();
+
 app.Run();
